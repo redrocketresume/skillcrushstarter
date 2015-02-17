a5883476de0498d6dc4dcf8bcd23f83b632503cf
Angela <angela@redrocketresume.com>
2015-02-17 14:57:34 -0700

    Adding my two cents to skillcrush

---
+++ PHPPage2.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>Hi, I'm Angela and I'm making a change to this repository.</title>
+    </head>
+    <body>
+        Here I am!
+    </body>
+</html>
